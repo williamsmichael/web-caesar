21eae6bd036011d4dab1f99c56da0eceee1819f0
mw <willia32@hotmail.com>
2017-06-18 21:35:44 -0700

    Create form with styles

--- main.py
+++ main.py
@@ -3,8 +3,41 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<DOCTYPE! html>
+<html>
+<head>
+    <style>
+        form, textarea {
+            width: 540px;
+        }
+        form {
+            background: #eee;
+            padding: 20px;
+            margin: 0 auto;
+            font: 16px sans-serif;
+            border-radius: 10px;
+        }
+        textarea {
+            margin: 10px 0;
+            height: 120px;
+        }
+    </style>
+</head>
+<body>
+    <form method='POST'>
+        <label>Rotate by:
+            <input type='text' name='rot' value=0>
+        </label>
+        <textarea name='text'></textarea>
+        <input type='submit' value='Submit Query'> 
+    </form>
+</body>
+</html>
+"""
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
 
 app.run()
